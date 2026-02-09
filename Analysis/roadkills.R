da2934c50542091970d29b357d33acab1a310054
Bibiana Terra Dasoler <84736570+bibianadasoler@users.noreply.github.com>
2026-02-09 11:56:31 -0300

    update

--- Analysis/roadkills.R
+++ Analysis/roadkills.R
@@ -46,7 +46,7 @@ control_ALLsegments <- data %>%
   pull(segment) %>%
   unique() %>%
   sort()
-control_ALLsegments <- sample(control_ALLsegments, size = 10, replace = F)
+
 
 # visualizing segments
 coords_km <- read.table(here::here("Analysis", "coords_km.txt"), h = T) %>% st_as_sf(., coords = c("x", "y"), crs = 32724)
@@ -140,7 +140,7 @@ df_predictions_complete_ALL$type <- factor(
     geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = 0.15) +
     facet_grid(~ type) + 
     scale_color_manual(values = c("Mitigated" = "firebrick", "Non-mitigated" = "black")) +
-    labs(x = "Years", y = "Number of roadkills") +
+    labs(x = "Years", y = "Number of roadkills (500 m)") +
     scale_y_continuous(limits = c(0, 3), breaks = c(0, 1, 2, 3)) +
     scale_x_continuous(limits = c(2004, 2017), breaks = c(2004, 2017), expand = c(0, 0)) +
     theme_minimal() + 
@@ -155,77 +155,5 @@ df_predictions_complete_ALL$type <- factor(
           plot.margin = margin(1.5, 15, 5, 5))
 )
 
-#ggsave(predicted_values_ALL, filename = here::here("Analysis", "CI_ALL.png"), 
+#ggsave(predicted_values_ALL, filename = here::here("Analysis", "CI_ALL2.png"), 
 #       dpi = 300, width = 2300, height = 1800, unit = "px")
-
-ggpubr::ggarrange(observed_values_ALL,
-                  predicted_values_ALL, 
-                  ncol = 2)
-ggsave(here::here("Analysis", "ALL_combined.png"), dpi = 300, width = 3000, height = 1780, unit = "px")
-
-
-ggpubr::ggarrange(observed_values_UCS,
-                  predicted_values_UCS,
-                  observed_values_ALL,
-                  predicted_values_ALL, 
-                  ncol = 2, nrow = 2)
-ggsave(here::here("Analysis", "control_impact.png"), dpi = 300, width = 3000, height = 2500, unit = "px")
-
-
-# # complete data - UCS control and impact
-# complete_UCS_Impact <- bind_rows(roadkills_UCScontrol, roadkills_impact) %>% mutate(year = as.numeric(year))
-# 
-# complete_glm_UCS <- glm.nb(roadkills ~ year * type, data = complete_UCS_Impact)
-# summary(complete_glm_UCS)
-# check_residuals(complete_glm_UCS)
-# Anova(complete_glm_UCS)
-# predictions_complete_UCS <- ggemmeans(complete_glm_UCS, terms = c("year", "type"))
-# plot(predictions_complete_UCS) 
-# df_predictions_complete_UCS <- as.data.frame(predictions_complete_UCS) %>% rename(year = x, type = group)
-# 
-# # figure
-# (observed_values_UCS <- ggplot(data = complete_UCS_Impact, aes(y = roadkills, x = year, color = type, shape = type)) +
-#     geom_point(position = position_jitterdodge(jitter.width = 0.2, jitter.height = 0.2, dodge.width = 0.6), 
-#                alpha = 0.5, size = 2) +
-#     scale_color_manual(values = c("control" = "firebrick", "impact" = "darkgreen"),
-#                        labels = c("Control", "Impact")) +
-#     scale_shape_manual(values = c("control" = 16, "impact" = 17),  # Formas personalizadas
-#                        labels = c("Control", "Impact")) +
-#     labs(x = "Years", y = "Number of roadkills", color = "Segment Type", shape = "Segment Type", title = "a) Observed values UCs") +
-#     theme_minimal() +
-#     theme(axis.title.x = element_text(size = 14),
-#           axis.title.y = element_text(size = 14),
-#           axis.text.x = element_text(size = 12),
-#           axis.text.y = element_text(size = 12),
-#           panel.grid.major.x = element_blank(),
-#           legend.position = "bottom") +
-#     scale_y_continuous(limits = c(-0.2, 10.1), breaks = c(0, 2, 4, 6, 8, 10)) +
-#     scale_x_continuous(limits = c(2003.7, 2017.4), breaks = c(2004, 2007, 2011, 2014, 2017)) 
-# )
-# 
-# (predicted_values_UCS <- ggplot(df_predictions_complete_UCS, aes(x = year, y = predicted, color = type, linetype = type)) +
-#     geom_line(aes(y = predicted), size = 1) +
-#     geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = 0.15, linetype = 0) +
-#     scale_color_manual(values = c("control" = "firebrick", "impact" = "darkgreen"),
-#                        labels = c("Control", "Impact")) +
-#     scale_linetype_manual(values = c("control" = "solid", "impact" = "solid"),
-#                           labels = c("Control", "Impact")) +
-#     labs(x = "Years", y = "Number of roadkills", color = "Segment type", linetype = "Segment type", title = "b) Predicted values UCs") +
-#     scale_y_continuous(limits = c(0, 3), breaks = c(0, 1, 2, 3)) +
-#     scale_x_continuous(limits = c(2004, 2017.2), breaks = c(2004, 2007, 2011, 2014, 2017), expand = c(0, 0)) +
-#     theme_minimal() + 
-#     theme(axis.title.x = element_text(size = 14),
-#           axis.title.y = element_text(size = 14, margin = margin(r = 10)),
-#           axis.text.x = element_text(size = 12, color = "grey10"),
-#           axis.text.y = element_text(size = 12, color = "grey10"),
-#           panel.grid.major.x = element_blank(),
-#           legend.position = "bottom")
-# )
-# 
-# #ggsave(predicted_values_UCS, filename = here::here("Analysis", "CI_UCS.png"), 
-# #       dpi = 300, width = 2100, height = 1780, unit = "px")
-# 
-# ggpubr::ggarrange(observed_values_UCS,
-#                   predicted_values_UCS, 
-#                   ncol = 2)
-#ggsave(here::here("Analysis", "UCS_combined.png"), dpi = 300, width = 3000, height = 1780, unit = "px")
